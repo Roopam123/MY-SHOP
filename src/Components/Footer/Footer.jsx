b7c5826d31eb711fa05340e595b59f0d8156e37f
Roopam123 <roopamr123@gmail.com>
2023-12-13 21:06:25 +0530

    Footer fixed

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -3,35 +3,38 @@ import './Footer.css'
 import compoy_log from '../../Assets/logo.png'
 import wattsapp from '../../Assets/whatsapp_icon.png'
 import pintester_icon from '../../Assets/../Assets/pintester_icon.png'
-import  insta from '../../Assets/instagram_icon.png'
+import insta from '../../Assets/instagram_icon.png'
+import { Link } from 'react-router-dom'
 
 function Footer() {
-  return (
-    <div className='footer'>
-        <div className="footer-brand">
-            <img src={compoy_log} alt="" />
-            <span>MY STORE</span>
+    return (
+        <div className='footer'>
+            <div className="footer-brand">
+                <Link to="/" style={{textDecoration:"none"}}>
+                    <img src={compoy_log} alt="" />
+                    <span>MY STORE</span>
+                </Link>
+            </div>
+            <div className="footer-mid">
+                <ul>
+                    <li>Compony</li>
+                    <li>Products</li>
+                    <li>offices</li>
+                    <li>About</li>
+                    <li>Contect</li>
+                </ul>
+            </div>
+            <div className="footer-end">
+                <img src={insta} alt="" />
+                <img src={pintester_icon} alt="" />
+                <img src={wattsapp} alt="" />
+            </div>
+            <dir className="footer-last">
+                <hr />
+                <span>Copyright @ 2023 - All Right Reserved</span>
+            </dir>
         </div>
-        <div className="footer-mid">
-            <ul>
-                <li>Compony</li>
-                <li>Products</li>
-                <li>offices</li>
-                <li>About</li>
-                <li>Contect</li>
-            </ul>
-        </div>
-        <div className="footer-end">
-            <img src={insta} alt="" />
-            <img src={pintester_icon} alt="" />
-            <img src={wattsapp} alt="" />
-        </div>
-        <dir className="footer-last">
-            <hr />
-            <span>Copyright @ 2023 - All Right Reserved</span>
-        </dir>
-    </div>
-  )
+    )
 }
 
 export default Footer
--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -5,23 +5,23 @@ import "./Navbar.css"
 import { Link } from 'react-router-dom'
 
 const Navbar = () => {
-  const [menu,setMenu] = useState("shop")
+  const [menu, setMenu] = useState("shop")
   return (
     <div className='navbar'>
       <div className='nav-logo'>
         <Link to='/'><img src={Logo} alt="logo" /></Link>
-        <Link to='/' style={{textDecoration:"none"}}><p className='logo-name'>MY STORE</p></Link>
+        <Link to='/' style={{ textDecoration: "none" }}><p className='logo-name'>MY STORE</p></Link>
       </div>
       <ul className='nav-menu'>
-        <li onClick={()=>setMenu("shop")}><Link to='/' className='nav-content' style={{textDecoration:'none'}}>Shop</Link> {menu==="shop"?<hr/>:<></>}</li>
-        <li onClick={()=>setMenu("man")}><Link to='/mans' style={{textDecoration:'none'}} className='nav-content'>Mens</Link>{menu==="man"?<hr/>:<></>}</li>
-        <li onClick={()=>setMenu("woman")}><Link to='/womans' style={{textDecoration:'none'}}
-        className='nav-content'>Womans</Link>{menu==="woman"?<hr/>:<></>}</li>
-        <li onClick={()=>setMenu("kids")}><Link to='/kids' style={{textDecoration:'none'}} className='nav-content'>Kids</Link> {menu==="kids"?<hr/>:<></>}</li>
+        <li onClick={() => setMenu("shop")}><Link to='/' className='nav-content' style={{ textDecoration: 'none' }}>Shop</Link> {menu === "shop" ? <hr /> : <></>}</li>
+        <li onClick={() => setMenu("man")}><Link to='/mans' style={{ textDecoration: 'none' }} className='nav-content'>Mens</Link>{menu === "man" ? <hr /> : <></>}</li>
+        <li onClick={() => setMenu("woman")}><Link to='/womans' style={{ textDecoration: 'none' }}
+          className='nav-content'>Womans</Link>{menu === "woman" ? <hr /> : <></>}</li>
+        <li onClick={() => setMenu("kids")}><Link to='/kids' style={{ textDecoration: 'none' }} className='nav-content'>Kids</Link> {menu === "kids" ? <hr /> : <></>}</li>
       </ul>
       <div className='login-card'>
-       <Link to='/login'><button className='login-btn'>Login</button></Link>
-        <Link to='/cart'><img src={Card} alt='card'/></Link>
+        <Link to='/login'><button className='login-btn'>Login</button></Link>
+        <Link to='/cart'><img src={Card} alt='card' /></Link>
         <div className='nav-card-count'>0</div>
       </div>
     </div>
