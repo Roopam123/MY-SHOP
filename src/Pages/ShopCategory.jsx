2466a3e40cc038becff6124b2f4389d806a74179
Roopam123 <roopamr123@gmail.com>
2023-12-14 00:05:57 +0530

    Inport value useing Context api

--- src/Pages/ShopCategory.jsx
+++ src/Pages/ShopCategory.jsx
@@ -1,9 +1,12 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import './CSS/ShopCategory.css'
+import {ShopContext} from '../Context/ShopContext'
 
-const ShopCategory = () => {
+const ShopCategory = (props) => {
+  const {all_product} = useContext(ShopContext)
   return (
-    <div>
-      <h1>ShopCategory</h1>
+    <div children="shop-category">
+
     </div>
   )
 }
