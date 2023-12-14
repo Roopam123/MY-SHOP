db03845529129e89aec5541b4fa796819b878d89
Roopam123 <roopamr123@gmail.com>
2023-12-14 16:03:04 +0530

    Complete Login Page

--- src/Pages/LoginSignUp.jsx
+++ src/Pages/LoginSignUp.jsx
@@ -1,8 +1,24 @@
 import React from 'react'
+import '../Pages/CSS/LoginSignUp.css'
 
 const LoginSignUp = () => {
   return (
-    <div>LoginSignUp</div>
+    <div className='loginsignnp'>
+      <div className='loginsignup-countainer'>
+        <h1>Sign Up</h1>
+        <div className='loginsignup-fields'>
+          <input type="text" placeholder='Enter Name'/>
+          <input type="email" placeholder='Enter Email'/>
+          <input type="password" placeholder='Enter Password' />
+        </div>
+        <button>Continue</button>
+        <p className='loginsignup-login'>Already have an account? <span>Login here</span></p>
+        <div className='loginsignup-agree'>
+          <input type="checkbox"/>
+          <p>By continuing, i agree to terms of use & policy </p>
+        </div>
+      </div>
+    </div>
   )
 }
 
